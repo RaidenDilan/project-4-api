d16eb3d0877ad0b559bf1c61979309d546a34ba3
Raiden Dilan <raiden18@me.com>
2017-05-20 02:25:09 +0100

    changed rack cors origins url in config/application to the new url

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Project4
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ['localhost:7000', 'https://desolate-sands-98689.herokuapp.com']
+        origins ['localhost:7000', 'https://palmtrees.herokuapp.com']
         resource '*', :headers => :any, :methods => :any
       end
     end
