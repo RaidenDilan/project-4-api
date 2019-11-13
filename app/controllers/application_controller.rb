397d4f7669dabb57d1dae729d9a322bf8fc7bf9b
RaidenDilan <raiden18@me.com>
2019-11-13 15:07:10 +0000

    API refactoring done - Time to deploy and see what's cookin'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::API
   before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery
   # protect_from_forgery with: :exception
   # protect_from_forgery with: :null_session
   # include Error::ErrorHandler
@@ -17,7 +16,7 @@ class ApplicationController < ActionController::API
 
   def current_user
     @current_user ||= User.find(decoded_token[:id]) if id_found?
-    
+
   rescue
     nil
   end
